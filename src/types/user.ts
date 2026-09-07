8a5ee9c799e804516f064859a26b70604e4375f5
1nyuszi <pal.balazs2@gmail.com>
2026-09-07 11:14:16 +0200

    Felhasználói interfész bővítése

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,6 +1,20 @@
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo?: Geo;
+}
+
 export interface Company {
   name: string;
   catchPhrase: string;
+  bs: string;
 }
 
 export interface User {
@@ -8,6 +22,7 @@ export interface User {
   name: string;
   username: string;
   email: string;
+  address?: Address;
   phone: string;
   website: string;
   company?: Company;
